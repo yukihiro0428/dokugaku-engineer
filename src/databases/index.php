13330999215fd0e4fe8aee2b74f403580b63debc
yukihiro kawakami <do_everything_in@icloud.com>
2020-10-11 10:20:16 +0900

    index.html修正

--- src/databases/index.php
+++ src/databases/index.php
@@ -1,1 +1,14 @@
-<?php echo 'hello';
+<?php require 'header.php'; ?>
+
+<section class="global-links">
+	<h3>HOMEページ</h3>
+	<p class="btn btn-primary btns"><a href="list.php">一覧ページへ</a></p>
+	<p class="btn btn-success btns"><a href="registration.php">登録ページへ</a></p>
+</section>
+
+<section class="frontPage">
+	<h4>アプリ概要</h4>
+	<p>内容内容内容内容内容内容内容内容内容内容内容内容内容内容</p>
+</section>
+
+<?php require 'footer.php'; ?>
